a753c6d600fe5f1fc6371a7a04633becf5d3d052
Cristina Silva <crissilva.eng@gmail.com>
2019-11-03 00:38:35 -0300

    refactor(scripts): change folder and adapt code to new structure

---
+++ scripts/example_player.kuhn.limit.3p.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(cd $(dirname $0); pwd -P)
+
+RESOURCES_PATH=$SCRIPT_PATH/../resources
+BINARIES_PATH=$SCRIPT_PATH/../out/bin
+
+$BINARIES_PATH/example_player $RESOURCES_PATH/kuhn.limit.3p.game $1 $2
